38f27e8d0f92260c56c3972f2ab68aabc73a06bf
Nicolas Cuervo <nicolas.cuervo@ettus.com>
2017-03-12 15:12:54 +0100

    install: removed shell change from install script

--- tools/install.sh
+++ tools/install.sh
@@ -80,19 +80,24 @@ main() {
   " ~/.zshrc > ~/.zshrc-omztemp
   mv -f ~/.zshrc-omztemp ~/.zshrc
 
+  # CUERVO sez: I removed the following lines because they conflict with my
+  # general Install script: https://github.com/primercuervo/dotfiles
+
+  # I change my shell within my script anyways
+
   # If this user's login shell is not already "zsh", attempt to switch.
-  TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-    # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
-    # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
-    fi
-  fi
+  #TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+  #if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+    ## If this platform provides a "chsh" command (not Cygwin), do it, man!
+    #if hash chsh >/dev/null 2>&1; then
+      #printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+      #chsh -s $(grep /zsh$ /etc/shells | tail -1)
+    ## Else, suggest the user do so manually.
+    #else
+      #printf "I can't change your shell automatically because this system does not have chsh.\n"
+      #printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+    #fi
+  #fi
 
   printf "${GREEN}"
   echo '         __                                     __   '
